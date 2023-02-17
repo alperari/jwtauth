130c374ed0f21987ef25cefb6cd6202b0673fe1e
Alper ARI <alper.hamza.ari@gmail.com>
2023-02-17 22:44:33 +0300

    Add mongoose connection

--- app.js
+++ app.js
@@ -2,15 +2,25 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 const PORT = process.env['PORT'] | 3000;
+const MONGO_URI = process.env['MONGO_URI'];
 
-app.listen(PORT);
-console.log('listening on port:', PORT);
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    app.listen(PORT);
+    console.log('mongoose connected succesfully.');
+    console.log('listening on port:', PORT);
 
-app.get('/', (req, res) => {
-  return res.json('OK');
-});
+    app.get('/', (req, res) => {
+      return res.json('OK');
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
