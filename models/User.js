a4a12fbad08cf61d7519cab4ed63b900db02d2c4
Alper ARI <alper.hamza.ari@gmail.com>
2023-02-19 23:46:42 +0300

    Add password hashing & User hooks

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
@@ -14,6 +15,21 @@ const userSchema = new mongoose.Schema({
     required: [true, 'Please provide password!'],
     minLength: [6, 'Password length cannot be less than 6!'],
   },
+});
+
+// Hooks
+userSchema.pre('save', async function (next) {
+  console.log('saving user:', this.email);
+
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
+
+userSchema.post('save', function (doc, next) {
+  console.log('saved user:', doc.email);
+  next();
 });
 
 const User = mongoose.model('user', userSchema);
